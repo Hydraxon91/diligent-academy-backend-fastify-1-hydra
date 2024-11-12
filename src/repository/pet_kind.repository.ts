3bf677ee9df625780cefa762fe0542c8c77c115e
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-11-12 19:03:46 +0100

    Fixes pet_kind queries

--- src/repository/pet_kind.repository.ts
+++ src/repository/pet_kind.repository.ts
@@ -17,7 +17,7 @@ export class KindRepository {
     }
   
     async read({ limit, offset }: { limit?: number, offset?: number } = {}) {
-      const sql = 'SELECT id, name, age, weight_in_kg, kind_id FROM pet_kind LIMIT $1 OFFSET $2;'
+      const sql = 'SELECT id, name FROM pet_kind LIMIT $1 OFFSET $2;'
       const rows = await this.client.query(sql, [limit, offset]) as Array<unknown>;
       return rows.map(this.toEntity)
     }
